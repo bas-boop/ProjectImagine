206c30e391f8b2359d2f542dc2e904e470604459
Bas de Reus <70578065+bas-boop@users.noreply.github.com>
2025-03-25 09:56:34 +0100

    Added instant scaling

---
+++ Assets/Scripts/Player/PlayerScaler.cs
@@ -0,0 +1,24 @@
+﻿using Framework.Extensions;
+using UnityEngine;
+
+namespace Player
+{
+    public sealed class PlayerScaler : MonoBehaviour
+    {
+        [SerializeField] private bool isInstant;
+        [SerializeField] private float smallerScale = 0.75f;
+
+        public void Scale()
+        {
+            if (isInstant)
+            {
+                Vector3 newScale = transform.localScale;
+                newScale.SetY(transform.localScale.y == 1 ? smallerScale : 1);
+                transform.localScale = newScale;
+                return;
+            }
+            
+            
+        }
+    }
+}
